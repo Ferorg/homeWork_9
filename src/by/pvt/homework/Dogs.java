df8b7cdce5a09892b21878cd993d66ff5e532a92
Ferorg <123727750+Ferorg@users.noreply.github.com>
2023-05-06 19:31:39 +0300

    hh

--- src/by/pvt/homework/Dogs.java
+++ src/by/pvt/homework/Dogs.java
@@ -1,20 +1,5 @@
 package by.pvt.homework;
 
 public class Dogs extends Animals {
-    private String name;
-    private String breed;
-    private int averageWeight;
-    private static int kollDogs;
-
-    public Dogs(String name, String breed, int averageWeight, String color, int maxLifespan, String foodType) {
-        super(color, maxLifespan, foodType);
-        this.name = name;
-        this.breed = breed;
-        this.averageWeight = averageWeight;
-    }
-
-    public void sing() {
-        System.out.println(name + " издайет громкий лай.");
-    }
 
 }
